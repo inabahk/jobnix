9869af80aa3941f83e7449aa72419ae88bdba78e
inabahk <inabahk@gmail.com>
2026-05-04 01:29:34 +0530

    fix user type index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { supabase } from '@/lib/supabase'
 import Link from 'next/link'
 
 export default function Home() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<any>(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
